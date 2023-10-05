4c4ed965f590a71610d61f10d57b254fa5522744
direraven22075 <hanhs9803753@gmail.com>
2023-10-05 19:38:25 +0900

    Work 2

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,4 @@
+public static class Constants
+{
+    public const float jumpForce = 5;
+}
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[
+    RequireComponent(typeof(Rigidbody2D),
+        typeof(SpriteRenderer)
+    )
+    ]
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float magnitude;
+    [SerializeField]
+    private float drag;
+    private Rigidbody2D rigid;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField]
+    private bool isGround = true;
+    private void Start()
     {
+        rigid = this.GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        PlayerMove();
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            rigid.AddForce(Vector2.up * Constants.jumpForce * 100);
+            isGround = false;
+        }
+    }
+    private void PlayerMove()
+    {
+        float ad = Input.GetAxis("Horizontal");
         
+        if (ad == 0)
+        {
+            rigid.AddForce(-(Vector2.ClampMagnitude(rigid.velocity, 1) * drag));
+        }
+        else
+        {
+            Vector2 vec = new Vector2(x: ad, 0) * speed;
+            rigid.AddForce(vec);
+            if (rigid.velocity.magnitude > magnitude)
+            {
+                rigid.velocity = Vector2.ClampMagnitude(rigid.velocity, 1) * magnitude;
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
     }
 }
