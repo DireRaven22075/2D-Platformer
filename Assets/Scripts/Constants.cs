8286e8a2871f19774222087da4a24382bcc756fd
direraven22075 <hanhs9803753@gmail.com>
2023-10-05 23:36:25 +0900

    work2 code cleaning

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -1,4 +1,15 @@
 public static class Constants
 {
-    public const float jumpForce = 5;
+    public struct Physics
+    {
+        public struct Player
+        {
+            public const float drag = 2;
+            public const float walkSpeed = 5;
+            public const float runSpeed = 8;
+            public const float maxSpeed = 10;
+            public const float force = 2;
+            public const float jumpForce = 5;
+        }
+    }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,58 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [
-    RequireComponent(typeof(Rigidbody2D),
+    RequireComponent(
+        typeof(CharacterController),
         typeof(SpriteRenderer)
-    )
-    ]
+    ),
+    AddComponentMenu("Mario/Player")
+]
 public class PlayerManager : MonoBehaviour
 {
-    [SerializeField]
-    private float speed;
-    [SerializeField]
-    private float magnitude;
-    [SerializeField]
-    private float drag;
+    #region Component
     private Rigidbody2D rigid;
+    #endregion
 
-    [SerializeField]
+    #region Variable
     private bool isGround = true;
-    private void Start()
+    #endregion
+
+    #region init
+    private void Awake()
     {
         rigid = this.GetComponent<Rigidbody2D>();
     }
+    private void Start()
+    {
+
+    }
+    #endregion
+
+    #region loop
     private void Update()
     {
-        PlayerMove();
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
-        {
-            rigid.AddForce(Vector2.up * Constants.jumpForce * 100);
-            isGround = false;
-        }
+        PlayerMove(Input.GetAxis("Horizontal"));
     }
-    private void PlayerMove()
+    private void LateUpdate()
     {
-        float ad = Input.GetAxis("Horizontal");
-        
-        if (ad == 0)
-        {
-            rigid.AddForce(-(Vector2.ClampMagnitude(rigid.velocity, 1) * drag));
-        }
-        else
-        {
-            Vector2 vec = new Vector2(x: ad, 0) * speed;
-            rigid.AddForce(vec);
-            if (rigid.velocity.magnitude > magnitude)
-            {
-                rigid.velocity = Vector2.ClampMagnitude(rigid.velocity, 1) * magnitude;
-            }
-        }
+
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    #endregion
+
+    #region Event
+    public void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.CompareTag("Ground"))
+        //if player get hit on ground
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isGround = true;
         }
     }
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        //if player get off 
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
+    }
+    #endregion
+
+    #region Function
+    private void PlayerMove(float ad)
+    {
+        //make player stop when input is nothing.
+        if (isGround && rigid.velocity.x != 0)
+        {
+            //Vector struct declare
+            Vector2 vec1 = new Vector2(
+                x: -rigid.velocity.x,
+                y: 0
+            );
+
+            //Add Resistive force TODO : Add Ice Function
+            rigid.AddForce(Vector2.ClampMagnitude(vec1, Constants.Physics.Player.drag));
+        }
+
+        //When user input jump TODO : Add Run and jump
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector2.up * Constants.Physics.Player.jumpForce);
+        }
+
+
+        if (ad == 0) return;
+        //make player move when user input anything TODO : Add run
+
+        //Vector2 변수 선언.
+        Vector2 vec2 = new Vector2(
+            x: ad,
+            y: 0
+        );
+
+        //이동방향에 대해 힘을 추가.
+        rigid.AddForce(vec2 * Constants.Physics.Player.walkSpeed);
+
+        //Clamp를 통해 최대 속도를 제한.
+        rigid.velocity = Vector2.ClampMagnitude(rigid.velocity, Constants.Physics.Player.maxSpeed);
+    }
+    #endregion
 }
